c6e28c2a4094f38cf01ceeb9363434ab1550f0c3
tonesto7 <tonesto7@gmail.com>
2018-08-06 17:52:38 -0400

    syncing

--- smartapps/tonesto7/pushover-manager.src/pushover-manager.groovy
+++ smartapps/tonesto7/pushover-manager.src/pushover-manager.groovy
@@ -15,6 +15,8 @@
  */
 
 import groovy.json.*
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.Charset
 def appVer() {"v1.0.20180801"}
 
 definition(
@@ -254,6 +256,8 @@ def filterPriorityMsg(msg, msgPr) {
     return [msg: msg, msgPr: msgPr]
 }
 
+include 'asynchttp_v1'
+
 void pushoverNotification(deviceName, msgData) {
     // log.debug "pushoverNotification($deviceName, $msgData)"
     if(deviceName && msgData) {
@@ -277,9 +281,16 @@ void pushoverNotification(deviceName, msgData) {
             if(msgData?.url_title) { bodyItems?.url_title = msgData?.urlTitle }
             if(msgData?.timestamp) { bodyItems?.timestamp = msgData?.timestamp }
             if(msgData?.html == true) { bodyItems?.html = 1 }
-            Map params = [uri: "https://api.pushover.net/1/messages.json"]
-            if(hasImage) {
-                String boundary = "----${(new Date())?.getTime()}"
+
+            def test = false
+            Map params = [uri: test ? "http://requestbin.fullcontact.com/r50ennr5" : "https://api.pushover.net/1/messages.json"]
+            String imgStr = hasImage ? getImageData(msgData?.image?.url, msgData?.image?.type) as String : null
+            log.debug "imgStr length: ${imgStr != null ? "${imgStr?.toString()?.length()}" : "null"}"
+            if(hasImage && imgStr) {
+                imgStr = new JsonOutput().toJson(imgStr) as String
+                imgStr = imgStr.replaceFirst("\"","")
+                imgStr = imgStr?.endsWith("\"") ? imgStr = removeLastChar(imgStr) : imgStr
+                String boundary = "pushover_${(new Date())?.getTime()}"
                 List bodyStr = []
                 bodyItems?.each { k, v ->
                     bodyStr?.push("--${boundary}")
@@ -291,90 +302,107 @@ void pushoverNotification(deviceName, msgData) {
                     bodyStr?.push("--${boundary}")
                     bodyStr?.push("Content-Disposition: form-data; name=\"attachment\"; filename=\"${msgData?.image?.name}\"")
                     bodyStr?.push("Content-Type: ${msgData?.image?.type}")
-                    bodyStr?.push("Content-Transfer-Encoding: base64")
+                    // bodyStr?.push("Content-Transfer-Encoding: binary")
                     bodyStr?.push("")
-                    // def img = "iVBORw0KGgoAAAANSUhEUgAAANIAAAAzCAYAAADigVZlAAAQN0lEQVR4nO2dCXQTxxnHl0LT5jVteHlN+5q+JCKBJITLmHIfKzBHHCCYBAiEw+I2GIMhDQ0kqQolIRc1SV5e+prmqX3JawgQDL64bK8x2Ajb2Bg7NuBjjSXftmRZhyXZ1nZG1eL1eGa1kg2iyua9X2TvzvHNN/Ofb2Z2ZSiO4ygZGZm+EXADZGSCgYAbICMTDATcABmZYCDgBsjIBAMBN0BGJhgIuAEyMsGA1wQdHZ1UV1cX5XK5qM7OzgcMRuNTrSbTEraq6strhdfzruTk5Wpz8q5c1l7Jyb6szc3K1l7RggtFxcWX2dvVB02mtmVOp3NIV2fnQFie2WyB5QS84TIy/YnXBFBI8BMM/pDqat0XzIVM08lTSVxyytn6jAuZV4FuzmtzclJz8/LT8vML0nJzr54HYkpLS88oTkxMMZ48mchlXrxUX1ffcBCUM8xms8lCkgk6pCT6aZvZvCrzYpbu2PfxHAg8l+obGmOt1vaJQBAPkvI5nM5fWyyWWTU1tfuA+IqOHDvGgehVCK4pA91oGZn+xluCAc0thtj4hCT72XOp9S0thi2FBQWPvb13z9RN61QH5s8NYxbMDct7KXyudt7MGeeWLFrwn8iVKz7auDZy3Z7dbzz91p43B8ZsjYLlDKmprd3/ffwpLjWNqbW32xcFuuEyMv2J2M1BJpMpKiExxZKZeamira1tvvqdt8OWL1l8asq4kNbRzz7NTRo7uuMPo4Y7Rz/zFBc64lluzHNDuZFDFe5PICx25/aY2B3bogf/dd9fKCA+CuytohOSkjuyLmtLXRwXGujGy8j0F8Qbdrt9bDpzQQ8jSHl5+dLt0VsOThgzwj7i6Se5kOHDuIljR9mXRrykjZj/wlVeSONHP8+FhykrJoeOsY8aNoQLAYJa9erShIPvvRsKhQTK/YleX3Pw5KlErpKt+iLQjZeR6S9IN35VXl75r3gw4HU6/Z6ojes/gMKAUQiKBQKiUvvLC1/MXL18WcKsaZOrJ4WObly7euUJsOQ7FjZ9Sh2IVC4oLhihZk6d1LB5/dpt+9R/hnuq4Xl5VwvT0jLKXS7XOHgaCAm0I2Rk+gL2os1mewXsiUw5uXlZn8T9LVI5ZWI1jEQTxozkgECgkDrmKqfrFy8ILwJ7om+3bNoQumTRwtDoqE0fTBsf2ggwg+jVBdOCT7eYwGfnti2bQXA6ME2nr9mbnHLOWV/fEI3WTdO0jMzdZjBAKWBwX8ojCqm8vOJoYvLp9qPfHTmy5rXlJ+BSbtzI5+5EI4ALRCTHHHpaQ8zWqOidO2IooBAKRKRDQDwGevJ4w8SQUR0e0bmB0QxEKh2IYsdbTW0zmIxM4/Wi4q9BfQMkCikCoAEUADgEeI3xOOVedkicp14e1V2uLwSpTwxNAPwRaGC7OQFqQp9xGDT+1ksUUubFrMoLFy/VL5g7+4ep48fa+P0Pz9jnn4H7JCcQBbP79V1rgJDmASE9um7NqvmxMdFbVateiwd7KKswHx+dwBKwzGq1jgDRrjQ7W5sB6hvsRUhQQCyh8Sg4xwW64/oTpUQ/CIm7xz652yg9flb40R+xIn5i/LWJKKSk5NOuwqIi7cSQkXooAD6ywE8YneDyLWrDuq/WR67+BvxcB5dtG9dGHgF7oZsgSuWFz555c0LISKcwIvHlAHSdnR0P37h5699pzIW6NrNlptFoIglJ7cOAgcTf40711nH3g5AguEH3/4YGaZPSj/6Ix/hGmKd/hXQqIanz5q1b8WA5VwOXdLwgoIjAsk2/Y1v0odUrXj0OT+vgNSCkjgXzZleANF3wpI6PRALxcDDt7BlTby+NWPgdqOPBisrKz8E+zFFXX79Sp9fjhKQiDAqjx6kRHmfCdHDWZek+zCp+gnac6i7XhxOSUkAExiZI7D32y73wtbKfy/CnPDdEISUkJjsrKiqPhocp86ZPGGeDSzkIWJa1Rq5ccXyDas1X8PBBuG9Cow8UE/yEaYYPeZybPnFcM1gGRh/6+KNhNbV1o7Mua29dysrOdblcQ4SvDHmMg5s/I2ZAxNP+bQz5zaVaABz0ij7kh6D7NVJnwL1NLJLXn47DCQmXjkXSqAnpFB4/CO2KkODjEE861B9i7VcKwPldgaQJQfKi4yFWkNZbPXzZuP4iQRobaLrBIhEpubP0xq2E9989MHnLpg3rX5hFlz3/1BMcWLaVRm/eeIieNL4KRhi450EjDxQOvAf2T+mrli9bDZaAq3Zu37b3nbf2zvnwg/d/DoRENbcYRmhzcn84n5peDkQ0FbNHUmMGjD/LtsGesnCi5GEEnYbLH+clP9ox6ABiRdKzmDz9ISR0wKgx7WJE7ILtxUUxlQQfGDFtQutC7cH1OUPIi8NbPWjZUtBgbIzApFMQhZSccrbrav61zAqWfWR79JbJ8+eG5Q97/HccfB0I/P4eEJADRigoJP6NBvgzBC715s2coTuwf9+0qI3rKbB3ooCQKCAkCgiJgkKCS7uWFuMbiUkpjpzcvCvg9yGIkFicwZiGeRMR7oQPB+x8VEy+5OcRDiDcoCdBErI/QsINdmH5pGiPAxUT6cQLxYjkY5D7aozdaiQNQ8iLoz+EhPY1i7FRg7ORKKTUtHSdVptTarPZhr737oFHgRj+7lmeVcRsjfrwxdkzc+DSDj50VU6Z0LR5/drDK5a8HLt4QfhusAfaBUQz8tDHHw/atE5FEhLkods6/ZfHjsdzZWXlJwRCGoxppAbTKG+gjeadoyZ0Duo43MbU6LmuJpTPCwk3WGFHqTyg9xiJbcIJSS2AtJkWG9R89Imgew8mI91zmcfQPfeo/D21iC9wdUZg2oaWoaG7xYvm59vFQ6qHt0EloQycb4WTN25cuttBFBKIRpfAsstkNpvD4Xtye9/802PLFi/6J1y6LXpx3mUQleJARHKCaGRbvWLZO1AwQEgUEBIFhOQWDRAS5UVIFOfinrheVHw2MTmFEwgJ1yAVxvFiKDBlaJA0uJmbrycEcw+3P0PTCDtOeJ1F8uKWCFL2fr5EOZzNOL+g0Qq9Lxz0IQQ7ceUKhSR2jzRxqb2Uj/MP46Ueb2WwyH1hREaPzln+HlFIjY1N+1NSzlirq/Wfg99/9saunVRszLaHdu3YHg32PueAOP4Klm8lk0JHt4GfZ6yPXE0tf2WxZCHZ7Q7K4XC667I77IuZC5nehIRzvBhqJD86s/KgM7CG7p4FUafh8pPsRAeFhu69SfWnjTgBisEi5aKDoQBjl7f9FSqgWBq/FPdVSIxIvTh/+Sok3OSI5kf7XbgvR/1yR2REIXV0dIRmX9beys7WljsdzhEeIQFBxFDLXl5E7doRMzFs+pTG+XNmFX726acPHo6Loz45fJhasmihG29CstraqfZ2+wCXyzWCZau+T0w63d9CQgcy6aACdRxDcJqKkJ9kp9Q9iK9tVGPyqQXgDkbg7wqCX6SgRmyAdmpo7w/JAyEk1Calj2WgYjOKXL8zsRKFBKNQA4hKp8+c62poaPwjfI0HLOfcX4WAYoqO2jQKLPVSdr++azsUkK9CagdCstnah14rvJ767XdHHSUlN64IhISbOdDO9IZYp4gNTIbGd7wCk1ch0jHodf4VJjGkHDig9nKYNLCDWSQN/3YD6hdWgl38JOLtpA9FTEg4f6JlqwX3pAoJTRMiUgZDKAP1HcyHTrgaYR4xIVFOp/PJgmuFFfngf52dnU+Q0nkDLuOsVitlb293Cwhib7dTFotlWloaU3s1vyANpHsUObVDHcISGt1XIWkIzpXSabhlli8zsD+oJdpGirRS/YIDd4LJeurCTX68WKQsqXA+E9qG+ho9FSSVIbwnVUgajB1olO8xEYgKCdLaaoouKv6hrNXYOt9ut8PlGAF3hMGWAa83NjVRNpDG4XDcwWg0rklLZ7iS0hufgXQDESHhliBCx3oDdUYBIR1LqAOtGxct0DqEHYd7eHg3hMRKbD9D8KvUZ3MqTFuFbVKI+AIdwDh/4soXTj5ouxkabyfJBl+E5G0f2isfUUjwD5RAzGbzQzW1dXOqdbphNbW1VE0NHp1OD6KOTVRI7UCIgusP6Gtq9iWnnOmqul0dhXkgi3M+BM5+pNOtELp7pvDWMRDcC4x8B6OzLzrgcLOssOPQAcuK2N0XIfXqVI9tqJB5+8Xa7Eu96IuwuP4Suyf0J85ejhYX0t2MSBTBHh4Vmp4opJYWgxujsZWqr2+ggJAoXY2eAoO/F/Ce1YYXkVBIMKKB5SJc0sGl3rC8/ALt2fNpzQ6HM9zVW0i4WVXoRP5ZjprufrbB0d0RBfccx0h3v8aCK1voWLTjOE+d/GsxJEeLzbAFdPdRMv/KUSwtfX+Es4ulex42kHzGd74Cc8/ouc8LXen5PV6QD62XEaRXENrrbVI00uIPvMWExHl8F0/37DeSDb4KieRHFpeeKCSDwegGCqmurt4tFn9E1CMigaWd52/jQX5fUlqakprOmMB/LzU3N+OEJNYgKc735agYfbPBl6f/pI5jfMgnNVr5UiYPuqxV+5CXFz4uAguFgFuKS53hSQj7UuzrD3x09LYXQ9vN0GQ/k8aOGpe+T0K6XV1NWaxWKYcNA1sMhgdANHLvgzo7u9zXK1n20PnzaVYQ8ZbB5SFBSPzszkp0vgLjEG+dyNL4iEBacvBovHQcFIeU42ZWpEP7KiTSS75qifmF/sS1lwc30H3pB1xkEgpJIZKfj5q4yOevkEjix054fgsJfu0BwkcZEqCs3zQ2Ne8pLin5urpad8hkaltQUnLjGbDfimQyLhjg298gDe7tb9Isoabx3wRV0/jXTvgBrfKkE+aLE8kjzCtcQvD5FB7UCLgyQgh288tTJSEfaVJB68QRQXt/N1GBaRuPmsY/OyP5UYov+DTCvBq65/JRCGq/AlM3tF+4xBSzQYncw7VPCOlhff8ICQqotq7OfRghWKphMZstaxKTUywnTp5qPHP2vOn0mXNcKpNhPpWYxKWmpjeDZd0WtG4vjZORuRcoafEI2QO/hASXdAajUcozpEGF14uPpgPhWK22xRaLdUbV7eo3b9ws28+yVXsdDvtceHonC0nmPoShey89ien9jkjNLQaqrc1MxASw2donpaZn1JeVlyeBfdEv2232O/sjMe4DJ8r8+GDo7i8K4va1KrH8PgsJPkuC+yL4tgL8JAGPucvKK2MzM7PaWltbl4AyB/wvj10Wksz9CCeCaDSC+CQkGInq6utF90Q8oIzf5l0tuFheXvkPsI962HN6JwtJ5n6FofEiwn3hsxeShVQF9kVQRPDfSZKwN6Kampt3Xiu83mQymcL5a/BrE1BMspBk7kNUdO8TVeGJoCiShOR+DaiuTvKfFQbpHqmoqMzW6/WJ8PgbOQ6XkQlKsBd5IUFaDAbJkQhitdpWgKUg226zLYS/y0KS+TGAvdjc3OKmqamFamtroywWq+gpHY/ZbBnU3GL4FHx+A8r5BeEhrYxM0BFwA2RkgoGAGyAjEwwE3AAZmWAg4AbIyAQDATdARiYYCLgBMjLBQMANkJEJBgJugIxMMPBfChd6NRZ5pkMAAAAASUVORK5CYII="
-                    // bodyStr?.push("${img}")
-                    bodyStr?.push("${getImageData(msgData?.image?.url, msgData?.image?.type)?.toString()}")
+                    bodyStr?.push("${imgStr}")
+                    bodyStr?.push("--${boundary}--")
                 }
-                bodyStr.push("--${boundary}--")
-                params?.contentType = "multipart/form-data; boundary=${boundary}"
-                params?.contentLength = bodyStr.join('\r\n')?.length()
+                params?.requestContentType = "multipart/form-data; boundary=${boundary}"
                 params?.body = bodyStr.join('\r\n')
-                // log.debug "${params?.body}"
             } else {
-                params?.contentType = "application/json"
+                params?.requestContentType = "application/json"
                 params?.body = new JsonOutput().toJson(bodyItems)
             }
-            
+            // log.debug "body (${params?.body?.toString()?.length()}): ${params?.body}"
             // log.debug "$params"
-            try {
-                httpPost(params) { resp ->
-                    def limit = resp?.getHeaders("X-Limit-App-Limit")
-                    def remain = resp?.getHeaders("X-Limit-App-Remaining")
-                    def resetDt = resp?.getHeaders("X-Limit-App-Reset")
-                    if(resp?.status == 200) {
-                        log.debug "Message Received by Pushover Server | Monthly Messages Remaining (${remain?.value[0]} of ${limit?.value[0]})"
-                        state?.messageData = [lastMessage: msgData?.message, lastMessageDt: formatDt(new Date()), remain: remain?.value[0], limit: limit?.value[0], resetDt: resetDt?.value[0]]
-                    } else if (resp?.status == 429) { 
-                        log.warn "Can't Send Notification... You have reached your (${limit?.value[0]}) notification limit for the month"
-                    } else {
-                        sendPush("pushoverNotification() ERROR: 'Pushover' received HTTP error ${resp?.status}. Check your keys!")
-                        log.error "Received HTTP error ${resp?.status}. Check your keys!"
-                    }
-                }
-            } catch (ex) {
-                if(ex instanceof groovyx.net.http.HttpResponseException && ex?.response) {
-                    log.error "pushoverNotification() HttpResponseException | Status: (${ex?.response?.status})${ex?.response?.data ? " | Data: ${ex?.response?.data}" : "" }"
-                } else {
-                    log.error "pushoverNotification Exception:", ex 
-                }
+            
+            asynchttp_v1.post(pushoverResponse, params, [hasImage: (hasImage && imgStr)])
+        }
+    }
+}
+
+public String removeLastChar(String s) {
+    return (s == null || s.length() == 0) ? null : (s.substring(0, s.length() - 1))
+}
+
+def pushoverResponse(resp, data) {
+    try {
+        Map headers = resp?.getHeaders()
+        def limit = headers["X-Limit-App-Limit"]
+        def remain = headers["X-Limit-App-Remaining"]
+        def resetDt = headers["X-Limit-App-Reset"]
+        if(resp?.status == 200) {
+            log.debug "Message Received by Pushover Server${(remain && limit) ? " | Monthly Messages Remaining (${remain} of ${limit})" : ""}"
+            state?.messageData = [lastMessage: msgData?.message, lastMessageDt: formatDt(new Date()), remain: remain, limit: limit, resetDt: resetDt]
+        } else if (resp?.status == 429) { 
+            log.warn "Couldn't Send Notification... You have reached your (${limit}) notification limit for the month"
+        } else {
+            if(resp?.hasError()) {
+                log.error "pushoverResponse: status: ${resp.status} | errorMessage: ${resp?.getErrorMessage()}"
+                // log.error "Received HTTP error ${resp?.status}. Check your keys!"
             }
         }
+    } catch (ex) {
+        if(ex instanceof groovyx.net.http.HttpResponseException && ex?.response) {
+            def rData = (ex?.response?.data && ex?.response?.data != "") ? " | Data: ${ex?.response?.data}" : ""
+            log.error "pushoverResponse() HttpResponseException | Status: (${ex?.response?.status})${rData}"
+        } else { log.error "pushoverResponse() Exception:", ex }
     }
 }
 
 def getImageData(url, fileType) {
     try {
-        def params = [uri: url, requestContentType: "${fileType}"]
+        def test = false
+        def params = [uri: url, contentType: test ? "${fileType}; charset=UTF-8" : "text/plain; charset=UTF-8"]
         httpGet(params) { resp ->
             if(resp?.status == 200) {
                 if(resp?.data) {
-                    Byte[] bytes = resp?.data?.getBytes()
-                    def size = resp?.getHeaders("Content-Length")
-                    if(size?.value && size?.value[0] && size?.value[0]?.isNumber()) {
-                        if(size?.value[0]?.toLong() > 2621440) {
-                            log.debug("FileSize: (${getFileSize(size?.value[0])})")
+                    // Byte[] rawBytes = resp?.data?.getBytes()
+                    // log.debug "encoding: ${resp?.getEntity()}"
+                    String rawText = ""
+                    def sizeHeader = resp?.getHeaders("Content-Length")
+                    def size = (sizeHeader?.value && sizeHeader?.value[0] && sizeHeader?.value[0]?.isNumber()) ? sizeHeader.value[0] : null
+                    if(size) {
+                        if(size?.toLong() > 2621440) {
+                            log.debug("FileSize: (${getFileSize(size)})")
                             log.warn "unable to encode file because it is larger than the 2.5MB size limit"
                             return null
+                        } else {
+                            StringReader sr = resp?.getData()
+                            for (int i = 0; i < size?.toInteger(); i++) { char c = (char) sr?.read(); rawText += c; }
+                            sr?.close()
+                            return rawText as String
+                            
+                            // def respData = resp?.data
+                            
+                            // int n = respData.available();
+                            // byte[] bytes = new byte[n];
+                            // respData.read(bytes, 0, n);
+                            // String s = new String(bytes, org.apache.commons.lang3.CharEncoding.UTF_8);
+                            
+                            // rawText = s.toString()
+                            
+                            // log.debug "respData: $respData"
+                            // ByteArrayOutputStream bos = new ByteArrayOutputStream()
+                            // int len
+                            // int sz = 4096
+                            // byte[] buf = new byte[sz]
+                            // while ((len = respData.read(buf, 0, sz)) != -1)
+                            //     bos.write(buf, 0, len)
+                            // buf = bos.toByteArray()
+                            // log.debug "buf: ${buf}"
+                            // log.debug "rawText: $rawText"
+                            // return rawText as String
                         }
-                    }
-                    // ByteArrayOutputStream buffer = new ByteArrayOutputStream()
-                    // InputStream is = new ByteArrayInputStream(bytes)
-                    // byte[] temp = new byte[1024];
-                    // int read;
-                    // while((read = is.read(temp)) >= 0){
-                    //     buffer.write(temp, 0, read)
-                    // }
-                    // byte[] data = buffer.toByteArray()
-                    InputStream input = new ByteArrayInputStream(bytes);
-                    int data = input.read();
-                    while(data != -1) {
-                        //do something with data
-                        data = input.read();
-                    }
-                    input.close();
-                    log.debug "data: $data"
-                    String enc = bytes?.encodeBase64() as String 
-                    return enc ? "data:${fileType};base64,${enc?.toString()}" : null 
+                    } else { return null }
                 }
             } else {
-                log.error("getImageData Resp: ${resp?.status} ${url}")
+                log.error("getImageData() Resp: ${resp?.status} ${url}")
                 return null
             }
         }
     } catch (ex) {
-        log.error "getImageData Exception:", ex
+        log.error "getImageData() Exception:", ex
         return null
     }
 }
